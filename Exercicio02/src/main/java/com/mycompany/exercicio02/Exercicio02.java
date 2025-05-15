e829f94e856d3aebd5749e6a277f8af60a90cb81
ArthurVeeck <arthur.silva.veeck@gmail.com>
2025-05-15 15:09:54 -0300

    Add exercicio 02

---
+++ Exercicio02/src/main/java/com/mycompany/exercicio02/Exercicio02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicio02;
+
+/**
+ *
+ * @author W10
+ */
+public class Exercicio02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
