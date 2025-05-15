b00137aca4d855b7d1ad1fa06f1dfe4f46c22c63
ArthurVeeck <arthur.silva.veeck@gmail.com>
2025-05-15 17:28:31 -0300

    Add exercicio 01

--- Exercicio01/src/main/java/com/mycompany/exercicio01/Exercicio01.java
+++ Exercicio01/src/main/java/com/mycompany/exercicio01/Exercicio01.java
@@ -2,20 +2,32 @@
 
 package com.mycompany.exercicio01;
 
+import java.util.Scanner;
+
 
 public class Exercicio01 {
-
+//Crie um método que receba um número inteiro e retorne true se 
+//for um número perfeito (a soma de seus divisores próprios é igual ao número, ex: 6 = 1 + 2 + 3).
     public static void main(String[] args) {
-        System.out.println("Teste");
+        Scanner ler = new Scanner (System.in);
+        System.out.println("Digite um numero: ");
+        int n1 = ler.nextInt();
+        int i = 0;
+        boolean resul = Perfeito(n1, true, i, i);
+        System.out.println("Seu numero é perfeito? "+ resul);
         
+    }
+    public static boolean Perfeito(int n1, boolean verificação, int i, int soma){
+        while(n1<n1){
+            soma = (n1+i);
+            i++;
+        }
+        if(n1==n1){
+            verificação = true;
+  }else{
+            verificação = false;
         
-        
-        
-        
-        
-        
-        
-        
-        
+}
+        return(verificação);
     }
 }
