c15f9d0c97d7524bcd28519fe2c879e254302240
ArthurVeeck <arthur.silva.veeck@gmail.com>
2025-05-20 16:35:53 -0300

    Add exercicio 03

---
+++ Exercicio03/src/main/java/com/mycompany/exercicio03/Exercicio03.java
@@ -0,0 +1,24 @@
+
+
+package com.mycompany.exercicio03;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+//        Crie um método que receba um array de inteiros 
+//        e retorne o maior valor absoluto presente no array.
+         int[] vetor = {6, 29, 10, 5, 1};
+         System.out.println(vetor[0]+" "+vetor[1]+" "+vetor[2]+" "+vetor[3]+" "+vetor[4]);
+         int resul = Maior(vetor);
+         System.out.println("O maior elemento é: "+ resul);
+    }
+    public static int Maior(int[] vetor){
+         Arrays.sort(vetor);
+        int Maiorzin = vetor[4];
+        return Maiorzin;
+    }
+}
